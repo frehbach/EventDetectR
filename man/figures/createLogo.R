9c45442c6247dae498b3436e63f7d83abb3bad7e
SteffenMoritz <steffen.moritz10@gmail.com>
2018-10-29 18:39:55 +0100

    Fix for Appveyor and Logo

---
+++ man/figures/createLogo.R
@@ -0,0 +1,8 @@
+library("hexSticker")
+
+x <- "picture.png"
+
+sticker(x, package="EventDetectR", p_family ="Arial",p_size = 6, p_color = "#FFFFFF",h_size = 0.5, h_fill = "#0000A2", h_color ="#000000", s_x=1.0, s_y=.75, s_width=0.65, s_height=0.8,
+        filename="eventdetectr-logo.png")
+
+sticker
