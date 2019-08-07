0ad326de91963ac28a41fe62e61805fe712d7cce
Frederik Rehbach <frederik.re@gmail.com>
2019-08-07 11:23:51 +0200

    Make test run faster for CRAN push

--- tests/testthat/test-detectionQuality.R
+++ tests/testthat/test-detectionQuality.R
@@ -7,8 +7,8 @@ test_that("confusionMatrix works",
               testthat::skip_on_os("windows")
 
               if(require("caret") & require("e1071")){
-                  train <- geccoIC2018Train[15000:17000,]
-                  edObject <- detectEvents(train[,-c(1,11)],windowSize = 500, nIterationsRefit = 200,verbosityLevel = 2,
+                  train <- geccoIC2018Train[16400:16600,]
+                  edObject <- detectEvents(train[,-c(1,11)],windowSize = 70, nIterationsRefit = 50,verbosityLevel = 2,
                                            postProcessorControl = list(nStandardDeviationsEventThreshhold = 3))
                   stat <- qualityStatistics(edObject, train$EVENT)
                   expect_equal(typeof(stat), "list")
